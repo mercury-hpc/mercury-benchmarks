2c73a91f0cae8341cdc5dbb20e200d41a4812f15
John Jenkins <jenkins@mcs.anl.gov>
2016-03-17 14:17:18 +0000

    script fixups, adding rpc+bulk support

--- runall-cooley.sh
+++ runall-cooley.sh
@@ -9,6 +9,7 @@ s="-s $((1<<24))"
 t="-t 5"
 a=""
 #a="-a"
+do_ctest1to3=no
 
 #concurrency params
 num_local_clients=4
@@ -25,10 +26,11 @@ if [[ -z $num_nodes || $num_nodes -eq 0 ]] ; then
     exit 1
 fi
 
-# two cases - all are separated or none are. Means that we either have a single
-# node alloc or >2 (rpc-server,bulk-server,client*)
-if [[ ! ( $num_nodes -eq 1 || $num_nodes -gt 2 ) ]] ; then
-    echo "ERROR: COBALT_NODEFILE must have either 1 node or >2" >&2
+# two cases for tests 1-3 - all are separated or none are.
+# Means that we either have a single node alloc or >2 
+# (rpc-server,bulk-server,client*)
+if [[ ! ( $do_ctest1to3 == no || $num_nodes -eq 1 || $num_nodes -gt 2 ) ]] ; then
+    echo "ERROR: COBALT_NODEFILE must have either 1 node or >2 to run tests 1-3" >&2
     exit 1
 fi
 
@@ -45,7 +47,7 @@ fi
 out_dir_pre=results.cooley/$results_subdir/ctest
 
 # create node port pairs - in single node case, use 4 clients, one server
-if [[ $num_nodes -eq 1 && 0 -eq 1 ]] ; then
+if [[ $num_nodes -eq 1 ]] ; then
     nodes=()
     for ((i = 0 ; i < $((num_local_clients+1)) ; i++)) ; do
         nodes[$i]=${nodes_raw[0]}:$((3344+i))
@@ -56,38 +58,37 @@ fi
 
 num_nodes=${#nodes[@]}
 
-mkdir -p ${out_dir_pre}1
-mkdir -p ${out_dir_pre}2
-mkdir -p ${out_dir_pre}3
+if [[ $do_ctest1to3 == yes ]] ; then
+    mkdir -p ${out_dir_pre}1
+    mkdir -p ${out_dir_pre}2
+    mkdir -p ${out_dir_pre}3
+    # run each test
+    nds="${nodes[0]} ${nodes[1]} ${nodes[2]}"
+    $rt -n 1 $s $nds
+    if [[ $? -ne 0 ]] ; then 
+        echo "ERROR: test 1 failed" >&2 
+        exit 1
+    fi
+    mv cli_send* ${out_dir_pre}1
+
+    $rt -n 2 $t $s $nds
+    if [[ $? -ne 0 ]] ; then 
+        echo "ERROR: test 2 failed" >&2 
+        exit 1
+    fi
+    mv cli_send* ${out_dir_pre}2
+
+    $rt -n 3 $t $s $nds
+    if [[ $? -ne 0 ]] ; then
+        echo "ERROR: test 3 failed" >&2 
+        exit 1
+    fi
+    mv cli_send* ${out_dir_pre}3
+fi
+
 mkdir -p ${out_dir_pre}4/rpc.1
+mkdir -p ${out_dir_pre}4/rpcbulk.1
 mkdir -p ${out_dir_pre}4/bulk.1
-mkdir -p ${out_dir_pre}4/rpc.2
-mkdir -p ${out_dir_pre}4/bulk.2
-mkdir -p ${out_dir_pre}4/rpc.$((num_nodes-1))
-mkdir -p ${out_dir_pre}4/bulk.$((num_nodes-1))
-
-# run each test
-nds="${nodes[0]} ${nodes[1]} ${nodes[2]}"
-$rt -n 1 $s $nds
-if [[ $? -ne 0 ]] ; then 
-    echo "ERROR: test 1 failed" >&2 
-    exit 1
-fi
-mv cli_send* ${out_dir_pre}1
-
-$rt -n 2 $t $s $nds
-if [[ $? -ne 0 ]] ; then 
-    echo "ERROR: test 2 failed" >&2 
-    exit 1
-fi
-mv cli_send* ${out_dir_pre}2
-
-$rt -n 3 $t $s $nds
-if [[ $? -ne 0 ]] ; then
-    echo "ERROR: test 3 failed" >&2 
-    exit 1
-fi
-mv cli_send* ${out_dir_pre}3
 
 # test 4 - 1 rpc client
 nds="${nodes[0]} ${nodes[1]}"
@@ -97,6 +98,14 @@ if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 mv cli_send* ${out_dir_pre}4/rpc.1
+
+# 1 rpcbulk client
+$rt -n 4 $t -b 0 -m rpcbulk $a $s $nds
+if [[ $? -ne 0 ]] ; then
+    echo "ERROR: test 4 (1 rpcbulk client) failed" >&2
+    exit 1
+fi
+mv cli_send* ${out_dir_pre}4/rpcbulk.1
 
 # 1 bulk client
 $rt -n 4 -b 1 $a $t $s $nds
@@ -109,6 +118,10 @@ mv cli_send* ${out_dir_pre}4/bulk.1
 # short circuit if doing single client/server runs
 [[ $num_nodes -le 2 ]] && exit 0
 
+mkdir -p ${out_dir_pre}4/rpc.2
+mkdir -p ${out_dir_pre}4/rpcbulk.2
+mkdir -p ${out_dir_pre}4/bulk.2
+
 nds="${nodes[0]} ${nodes[1]} ${nodes[2]}"
 # two-client runs
 $rt -n 4 $t -b 0 $a $s $nds
@@ -117,6 +130,12 @@ if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 mv cli_send* ${out_dir_pre}4/rpc.2
+$rt -n 4 $t -b 0 -m rpcbulk $a $s $nds
+if [[ $? -ne 0 ]] ; then
+    echo "ERROR: test 4 (2 rpcbulk clients) failed" >&2
+    exit 1
+fi
+mv cli_send* ${out_dir_pre}4/rpcbulk.2
 $rt -n 4 -b 1 $a $t $s $nds
 if [[ $? -ne 0 ]] ; then
     echo "ERROR: test 4 (1,1 rpc,bulk clients) failed" >&2
@@ -127,8 +146,20 @@ mv cli_send* ${out_dir_pre}4/bulk.2
 # short circuit if doing two client runs
 [[ $num_nodes -le 3 ]] && exit 0
 
+mkdir -p ${out_dir_pre}4/rpc.$((num_nodes-1))
+mkdir -p ${out_dir_pre}4/rpcbulk.$((num_nodes-1))
+mkdir -p ${out_dir_pre}4/bulk.$((num_nodes-1))
+
 # test 4 - N rpc clients, 0 bulk
 $rt -n 4 -b 0 $a $t $s ${nodes[@]}
+if [[ $? -ne 0 ]] ; then
+    echo "ERROR: test 4 ($((num_nodes-1)) rpc clients) failed" >&2
+    exit 1
+fi
+mv cli_send* ${out_dir_pre}4/rpc.$((num_nodes-1))
+
+# test 4 - N rpcbulk clients
+$rt -n 4 -b 0 -m rpcbulk $a $t $s ${nodes[@]}
 if [[ $? -ne 0 ]] ; then
     echo "ERROR: test 4 ($((num_nodes-1)) rpc clients) failed" >&2
     exit 1
